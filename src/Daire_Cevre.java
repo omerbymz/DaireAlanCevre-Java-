be713a301a330d0e2ee883f8f5951d74a249a0c9
Omercan Baymaz <obaymaz36@gmail.com>
2022-02-25 14:46:27 +0300

    Daire Alan Cevre Hesaplama

---
+++ src/Daire_Cevre.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Daire_Cevre {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double yaricap;
+        System.out.print("Lütfen bir yarıçap giriniz: ");
+        yaricap=input.nextInt();
+        double alan;
+        alan= yaricap*yaricap*3.14;
+        double cevre;
+        cevre=yaricap*3.14*2;
+        System.out.println("Dairenin alanı : " + alan);
+        System.out.println("Dairenin çevresi : " + cevre);
+
+    }
+}
